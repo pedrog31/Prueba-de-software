abf8c89259bcb8f6164d1945b93074e565d4fb87
mario.garciat <andrex044@gmail.com>
2017-08-15 17:48:57 -0500

    la wea de calculo

--- Programa1/src/programa1/Calculo.java
+++ Programa1/src/programa1/Calculo.java
@@ -10,67 +10,77 @@ package programa1;
  * @author mario.garciat
  */
 public class Calculo {
-//    
-//    private Lista listaDatos, listaVacía;
-//    private Nodo nodoMedia, nodoDesv;
-//    private float media1, media2, desvEst1, desvEst2;
-//    
-//    public Nodo calcularMedia(Lista lista){
-//        media1 = 0;
-//        media2 = 0;
-//        int count = 0;
-//        nodoMedia = new Nodo();
-//        Nodo actual = lista.cabeza;
-//        if(actual.equals(null)){
-//            System.out.println("Parámetro inválido");
-//        }else if(actual.primero.equals(null)){
-//            System.out.println("Lista vacía, ingrese datos");
-//        }else{
-//            while(!actual.equals(null)){
-//                actual = actual.primero;
-//                media1 = media1 + actual.getDato1;
-//                media2 = media2 + actual.getDato2;
-//                count += 1;
-//                actual = actual.siguiente;
-//            }
-//        }
-//        media1 = media1/count;
-//        media2 = media2/count;        
-//        nodoMedia.setDato1(media1);
-//        nodoMedia.serDato2(media2);
-//        return nodoMedia;
-//    }
-//    
-//    public Nodo calcularDesvEst(Lista lista){
-//        desvEst1 = 0;
-//        desvEst2 = 0;
-//        nodoDesv = new Nodo();
-//        nodoMedia = calcularMedia(lista);
-//        media1 = nodoMedia.getDato1;
-//        media2 = nodoMedia.getDato2;
-//        int count = 0;
-//        Nodo actual = lista.cabeza;
-//       /* if(actual.equals(null)){
-//            System.out.println("Parámetro inválido");
-//        }else if(actual.primero.equals(null)){
-//            System.out.println("Lista vacía, ingrese datos");
-//        }else{*/
-//        while(!actual.equals(null)){
-//            actual = actual.primero;
-//            desvEst1 = desvEst1 + Math.pow((actual.getDato1 - media1),2);
-//            desvEst2 = desvEst2 + Math.pow((actual.getDato2 - media1),2);
-//            count += 1;
-//            actual = actual.siguiente;
-//          
-//        }
-//        if(count == 1){
-//            System.out.println("La lista debe tener más de un dato para calcular la desviación estandar");
-//        }else{
-//            desvEst1 = (float) Math.sqrt(desvEst1/(count-1));
-//            desvEst2 = (float) Math.sqrt(desvEst2/(count-1));
-//        }
-//        nodoDesv.setDato1(desvEst1);
-//        nodoDesv.setDato2(desvEst2);
-//        return nodoDesv;
-//    }
+    
+    private Lista listaDatos, listaVacía;
+    private Nodo nodoMedia, nodoDesv;
+    private NodoCabeza cabeza;
+    private float media1, media2, desvEst1, desvEst2;
+    
+    public Nodo calcularMedia(Lista lista){
+        media1 = 0;
+        media2 = 0;
+        int count = 0;
+        nodoMedia = new Nodo();
+        Nodo actual;
+        if(lista == null){
+            System.out.println("Parámetro inválido");
+            return null;
+        }
+        cabeza = lista.getCabeza();
+        if(cabeza.getLigaInicio() == null){
+            System.out.println("Lista vacía, ingrese datos");
+        }else{
+            actual = cabeza.getLigaInicio();
+            while(actual != null){
+                media1 = media1 + actual.getColumna1();
+                media2 = media2 + actual.getColumna2();
+                count += 1;
+                actual = actual.getLiga();
+            }
+            media1 = media1/count;
+            media2 = media2/count;        
+            nodoMedia.setColumna1(media1);
+            nodoMedia.setColumna2(media2);
+        }
+        
+        return nodoMedia;
+    }
+    
+    public Nodo calcularDesvEst(Lista lista){
+        desvEst1 = 0;
+        desvEst2 = 0;
+        nodoDesv = new Nodo();
+        Nodo actual;
+        nodoMedia = calcularMedia(lista);
+        if(nodoMedia == null){
+            System.out.println("Parámetro inválido");
+            return null;
+        }
+        media1 = nodoMedia.getColumna1();
+        media2 = nodoMedia.getColumna2();
+        int count = 0;
+        cabeza = lista.getCabeza();
+        if(cabeza.getLigaInicio() == null){
+            System.out.println("Lista vacía, ingrese datos");
+        }else{
+            actual = cabeza.getLigaInicio();
+            while(actual != null){
+                desvEst1 = desvEst1 + (float)Math.pow((actual.getColumna1()-media1),2);
+                desvEst2 = desvEst2 + (float)Math.pow((actual.getColumna2()-media2),2);
+                count += 1;
+                actual = actual.getLiga();
+            }
+            if(count == 1){
+                System.out.println("La lista debe tener más de un dato para calcular la desviación estandar");
+            }else{
+                desvEst1 = (float) Math.sqrt(desvEst1/(count-1));
+                desvEst2 = (float) Math.sqrt(desvEst2/(count-1));
+                desvEst1 = (float)Math.rint((double)desvEst1);
+                desvEst2 = (float)Math.rint((double)desvEst2);
+            }
+            nodoDesv.setColumna1(desvEst1);
+            nodoDesv.setColumna2(desvEst2);
+        }
+        return nodoDesv;
+    }
 }
---
+++ Programa1/test/programa1/PruebaCalculo.java
@@ -0,0 +1,112 @@
+package programa1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pedrog31
+ */
+public class PruebaCalculo{
+    
+    private Lista lista, listaVacía;
+    private Nodo nodo, nodoMedia, media, desvEst, nodoDesv, nodoVacio;
+    private Calculo c;
+    private Lector l;
+    
+    
+    public PruebaCalculo() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        lista = new Lista();
+        listaVacía = new Lista();
+        c = new Calculo();
+        
+        nodo = new Nodo(160f,15.0f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(591f,69.9f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(114f,6.5f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(229f,22.4f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(230f,28.4f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(270f,65.9f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(128f,19.4f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(1657f,198.7f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(624f,38.8f);
+        lista.insertarNodo(nodo);
+        nodo = new Nodo(1503f,138.2f);
+        lista.insertarNodo(nodo);
+           
+        nodoVacio = new Nodo();
+        nodoMedia = new Nodo (550.6f,60.32f);
+        nodoDesv = new Nodo (572.03f,62.26f);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCalcularMediaListaVacia () {
+        media = c.calcularMedia(listaVacía);
+        assertFalse(!(media.getColumna1()==(float)0 && (media.getColumna2()==(float)0)));
+    }
+    
+    @Test
+    public void testCalcularMediaNull () {
+        media = c.calcularMedia(null);
+        assertNull(media);
+    }
+    
+    @Test
+    public void testCalcularMedia () {
+        media = c.calcularMedia(lista);
+        assertFalse(!(media.getColumna1()==550.6f && (media.getColumna2()==60.32f)));
+    }
+    
+    @Test
+    public void testCalcularDesvEstListaVacia () {
+        desvEst = c.calcularDesvEst(listaVacía);
+        assertFalse(!(desvEst.getColumna1()==(float)0 && desvEst.getColumna2()==(float)0));
+    }
+    
+    @Test
+    public void testCalcularDesvEstNull () {
+        desvEst = c.calcularDesvEst(null);
+        assertNull(desvEst);
+    }
+    
+    @Test
+    public void testCalcularDesvEst () {
+        desvEst = c.calcularDesvEst(lista);
+        assertFalse(!(desvEst.getColumna1()==572.03f && (desvEst.getColumna2()==62.26f)));
+    }
+    
+}
