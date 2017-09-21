ffd75db37df05938a9e0611690d3e6092b547fcf
Daniel Ospina <daniel.ospinaperez@gmail.com>
2017-09-20 22:45:32 -0500

    convierte strings a numeros

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -19,18 +19,46 @@ var router = express.Router();
 
 router.post('/', upload.single('datos'), function(req, res, next) {
     var datos = req.file.buffer.toString();
+    var matriz = leerArchivo(datos);
+
+
+    res.send(matriz);
+});
+
+function leerArchivo(datos) {
     var lineas = datos.split('\n');
     var matriz = [];
     for(var i = 0; i < lineas.length-1; i++){
-        var fila = lineas[i].split(' ');
+        var fila = stringToFloatArray(lineas[i]);
         console.log(fila);
         matriz[i] = fila;
     }
 
     //TODO: Hacer que se guarden como numeros y no como strings
 
-    res.send(matriz);
-});
+    return matriz;
+}
+
+function stringToFloatArray(linea) {
+    var error = false;
+    var fila = linea.split(' ');
+    for (var i = 0; i < fila.length; i++) {
+        fila[i] = parseFloat(fila[i]);
+        if(isNaN(fila[i])){
+            error = true;
+        }
+    }
+
+    if (error) {
+        errorMalFormato();
+    } else{
+        return fila;
+    }
+}
+
+function errorMalFormato() {
+
+}
 
 
 
